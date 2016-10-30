3e590e1d14e061d93390a8fc6b2fc911a892fd5a
bigbigda <iagp1991@163.com>
2016-10-30 15:02:08 +0800

    add the new project

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,103 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QtWidgets>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow),
+    imageLabel(new QLabel)
 {
     ui->setupUi(this);
+    ui->scrollArea->setBackgroundRole(QPalette::Base);
+    ui->scrollArea->setWidget(imageLabel);
+    ui->scrollArea->setVisible(false);
+
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+static void initializeImageFileDialog(QFileDialog &dialog, QFileDialog::AcceptMode acceptMode)
+{
+    static bool firstDialog = true;
+
+    if (firstDialog) {
+        firstDialog = false;
+        const QStringList picturesLocations = QStandardPaths::standardLocations(QStandardPaths::PicturesLocation);
+        dialog.setDirectory(picturesLocations.isEmpty() ? QDir::currentPath() : picturesLocations.last());
+    }
+
+    QStringList mimeTypeFilters;
+/*    const QByteArrayList supportedMimeTypes = acceptMode != QFileDialog::AcceptOpen
+        ? QImageReader::supportedMimeTypes() : QImageWriter::supportedMimeTypes();
+    foreach (const QByteArray &mimeTypeName, supportedMimeTypes)
+        mimeTypeFilters.append(mimeTypeName);
+        mimeTypeFilters.sort();
+        dialog.setMimeTypeFilters(mimeTypeFilters);
+        dialog.selectMimeTypeFilter("image/jpeg");
+        if (acceptMode == QFileDialog::AcceptSave)
+        dialog.setDefaultSuffix("xxxx");
+*/
+}
+
+bool MainWindow::loadFile(const QString &fileName)
+{
+    QImageReader reader(fileName);
+    reader.setAutoTransform(true);
+    const QImage newImage = reader.read();
+    if (newImage.isNull()) {
+        QMessageBox::information(this, QGuiApplication::applicationDisplayName(),
+                                 tr("Cannot load %1: %2")
+                                 .arg(QDir::toNativeSeparators(fileName), reader.errorString()));
+        return false;
+    }
+//! [2]
+
+    setImage(newImage);
+
+    setWindowFilePath(fileName);
+
+    const QString message = tr("Opened \"%1\", %2x%3, Depth: %4")
+        .arg(QDir::toNativeSeparators(fileName)).arg(image.width()).arg(image.height()).arg(image.depth());
+    statusBar()->showMessage(message);
+    return true;
+}
+
+void MainWindow::setImage(const QImage &newImage)
+{
+    image = newImage;
+    imageLabel->setPixmap(QPixmap::fromImage(image));
+//! [4]
+    scaleFactor = 1.0;
+
+    ui->scrollArea->setVisible(true);
+    ui->actionA->setEnabled(true);
+    //fitToWindowAct->setEnabled(true);
+    //updateActions();
+
+    //if (!fitToWindowAct->isChecked())
+     //   imageLabel->adjustSize();
+}
+
+void MainWindow::on_action1_triggered()
+{
+  QFileDialog dialog(this, tr("Open File!!!!!"));
+  initializeImageFileDialog(dialog, QFileDialog::AcceptOpen);
+  while (dialog.exec() == QDialog::Accepted && !loadFile(dialog.selectedFiles().first())) {}
+}
+
+
+
+void MainWindow::on_actionA_triggered()
+{
+
+}
+
+void MainWindow::on_actionA_2_triggered()
+{
+    QMessageBox::aboutQt(this);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,15 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QImage>
 namespace Ui {
 class MainWindow;
 }
+class QAction;
+class QLabel;
+class QMenu;
+class QScrollArea;
+class QScrollBar;
 
 class MainWindow : public QMainWindow
 {
@@ -14,9 +19,23 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+private:
+     bool loadFile(const QString &);
+     void setImage(const QImage &newImage);
+
+private slots:
+    void on_action1_triggered();
+   // void open();
+    void on_actionA_triggered();
+
+    void on_actionA_2_triggered();
 
 private:
     Ui::MainWindow *ui;
+    QImage image;
+    QLabel *imageLabel;
+    double scaleFactor;
+
 };
 
 #endif // MAINWINDOW_H
